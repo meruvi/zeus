e1c1917cb279d8864c166f5a19b4b533b18ee209
meruvi <gmeruviarojas@gmail.com>
2019-06-16 11:16:28 -0400

    Creación de logos e iconos

---
+++ app/src/main/java/com/example/navigationdrawer/SplashScreenActivity.java
@@ -0,0 +1,41 @@
+package com.example.navigationdrawer;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+//        MainV7 mainV7=new MainV7();
+//        mainV7.init(getApplicationContext());
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent().setClass(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
